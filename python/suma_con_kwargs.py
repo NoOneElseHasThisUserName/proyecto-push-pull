b7ea634ef8c356f1ea9ae0cb6206d5d2bae28595
NoOneElseHasThisUserName <73353651+NoOneElseHasThisUserName@users.noreply.github.com>
2023-08-23 11:35:03 +0200

    Añadida nota explicativa acerca de **kwargs

--- python/suma_con_kwargs.py
+++ python/suma_con_kwargs.py
@@ -3,6 +3,13 @@ def suma_con_kwargs(**kwargs):
     return resultado
 
 
+'''
+NOTA Acerca de **kwargs
+
+En **kwargs se recibe un número indeterminado de argumentos desde cualquier
+variable de tipo dict
+'''
+
 numeros = {'num1': 5, 'num2': 10, 'num3': 15, 'num4': 20}
 resultado = suma_con_kwargs(**numeros)
 print(f"La suma es: {resultado}")
